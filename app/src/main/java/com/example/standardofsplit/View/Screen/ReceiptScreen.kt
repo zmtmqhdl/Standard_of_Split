ec7fa9ac37e4e5e5f57256474a1a4d28ccb861a1
zmtmqhdl <zmtmqhdl@naver.com>
2024-10-21 00:43:38 +0900

    making start button design like toss

--- app/src/main/java/com/example/standardofsplit/View/Screen/ReceiptScreen.kt
+++ app/src/main/java/com/example/standardofsplit/View/Screen/ReceiptScreen.kt
@@ -1,9 +1,17 @@
 package com.example.standardofsplit.View.Screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.standardofsplit.Model.ReceiptClass
 import com.example.standardofsplit.View.Activity.CalculatorActivity
 import com.example.standardofsplit.View.Components.BTN_Basic
@@ -17,7 +25,17 @@ fun ReceiptScreen(
     val receipts = receipt.receipts.observeAsState(initial = emptyList())
 
     ReceiptDetailList(receipts = receipts.value)
-    BTN_Basic(content = "정산시작쓰", onClick = intentToCalculatorActivity)
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        BTN_Basic(
+            content = "정산시작쓰",
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+                .fillMaxWidth(),
+            onClick = intentToCalculatorActivity
+        )
+    }
 }
 
 @Composable
