4f8693bf6d08b62f9484bbafbf3f6dde166f269e
zmtmqhdl <zmtmqhdl@naver.com>
2024-10-26 18:40:23 +0900

    Dialog work now

--- app/src/main/java/com/example/standardofsplit/View/Components/Dailog.kt
+++ app/src/main/java/com/example/standardofsplit/View/Components/Dailog.kt
@@ -12,9 +12,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,7 +23,6 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
-import java.time.format.TextStyle
 
 @Composable
 fun Receipt_Add_Dialog(
